5eea99217dc6099f4a6946eca9ce80c2a57cb9ef
EVGENY KVEST <evgenykvest@gmail.com>
2025-01-07 17:06:22 +0100

    Firts Commit & AUFGABE_1 DESIGN PATTERN

---
+++ src/ConfigurationManager.java
@@ -0,0 +1,64 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class ConfigurationManager  {
+
+
+    private static volatile ConfigurationManager instance;
+    private final Map <String, String> properties;
+
+    private ConfigurationManager(){
+        properties = new HashMap<>();
+
+    }
+
+    public static ConfigurationManager getInstance(){
+        if(instance == null){
+            instance = new ConfigurationManager();
+            synchronized (ConfigurationManager.class){
+                if(instance == null){
+                    instance = new ConfigurationManager();
+                }
+            }
+        }
+        return instance;
+    }
+
+
+    public void setProperty(String key, String value){
+        properties.put(key, value);
+    }
+
+    public String getProperty(String key){
+        return properties.getOrDefault(key, "Kein Schlüssel");
+    }
+
+    public void printAllProperties(){
+        if(properties.isEmpty()){
+            System.out.println("Keine Konfigurationen gespeichert");
+        } else {
+            System.out.println(properties);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        ConfigurationManager configManager = ConfigurationManager.getInstance();
+
+        configManager.setProperty("1", "XRP");
+        configManager.setProperty("2", "XLM");
+        configManager.setProperty("3", "HBAR");
+
+        System.out.println("Best Coin: " + configManager.getProperty("2"));
+        System.out.println("Alle Konfigurationen:");
+        configManager.printAllProperties();
+
+        // Checken, ob alles die gleiche Instanz hat
+        System.out.println(ConfigurationManager.getInstance().toString());
+        System.out.println(ConfigurationManager.getInstance().toString());
+
+    }
+}
+
+
+
